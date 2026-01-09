92c843750597db0c7b9941415af353cfd01e31f3
[Dany] <[dany.ssimon2007@yandex.ru]>
2026-01-09 04:21:19 +0300

    Updates webpack build hashes for hot module replacement
    Regenerates output hashes and hot-update files to reflect recent style or source changes, ensuring consistency and accurate hot reloading during development.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5b0fd380087ead75"; }
+/******/ 		__webpack_require__.h = function() { return "5ee92eb6df148d97"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
